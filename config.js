115858ac428d05e979fa7d3bbaa7d6a9a96c72ef
tkay23 <158102238+tkay23@users.noreply.github.com>
2024-01-30 01:35:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const chalk = require("chalk");
 
 //
 global.available = true;
-global.autoReadAll = false;
+global.autoReadAll = true;
 global.antitags = true;
 
 
@@ -22,12 +22,12 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 //
 global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94772108460"];
-global.OwnerName = "KUSHAN";
-global.BotName = "DARK SHAM";
-global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
-global.author = "KUSHAN";                               //Do not change.
+global.OwnerNumber = ["+263776328801", "+263776328801"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+263776328801"];
+global.OwnerName = "TAKUNDA DUBE";
+global.BotName = "TAKUNDA DUBE";
+global.packname = "🦋TAKUNDA DUBE🦋";                             //Do not change.
+global.author = "TAKUNDA DUBE";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
@@ -41,12 +41,12 @@ global.openAiAPI = "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";
 
 
 //
-global.location = "mathara, Sri Lankan";
+global.location = "Bulawayo, Zimbabwe";
 global.reactmoji = "💞";
 global.themeemoji = "😂";
 global.vidmenu = { url: 'https://tenor.com/en-CA/view/anonymous-hacker-smooth-gif-10171911193797826570%9D%90%95-12-14' };
 global.websitex = "https://github.com/CYBER-x-SACHIYA-SL-MD-BOT/CYBER-PANDA-MD.V.0.4";
-global.lolhuman = "kushan";
+global.lolhuman = "Takunda";
 
 
 //
